e852fb38572794ad439fad87902c64273d8fc09f
dguarino <domenico.guarino@gmail.com>
2016-10-11 18:26:18 +0200

    pure dict parameters

--- params.py
+++ params.py
@@ -1,19 +1,5 @@
 
-
-p_c = .02
-b = 0.1
-rng = NumpyRNG(1235342134, parallel_safe=False)
-g_e = 6e-3         # nS
-g_i = 67e-3        # nS
-g_ext = 6e-3
-stim_dur = 50.0
-p_c = .02
-#scale_factor = round((py_n+inh_n) / (pyB_n+inhB_n))
-inter_p_c = .01
-v_init = -60.0
-
-
-params = {
+{
 
     'DistanceDep': True,
     'run_time': 1000, # ms
@@ -26,7 +12,7 @@ params = {
             'cellparams' : {
                 'start':0.0,
                 'rate':50.,
-                'duration':stim_dur
+                'duration':50.0
             }
         }
        'py' : {
@@ -44,7 +30,7 @@ params = {
                 'tau_w'      : 600.0,
                 'cm'         : 0.200,
                 'a'          : 0.001e3,
-                'b'          : b
+                'b'          : .1
             }
         },
         'inh' : {
@@ -72,35 +58,35 @@ params = {
             'source' : 'ext',
             'target' : 'py',
             'connector' : FixedProbabilityConnector(.02),
-            'synapse_type' : StaticSynapse(weight=g_ext),
+            'synapse_type' : StaticSynapse(weight=6e-3),
             'receptor_type' : 'excitatory'
         },
         'py_py' : {
             'source' : 'py',
             'target' : 'py',
-            'connector' : FixedProbabilityConnector(p_c, allow_self_connections=False, rng=rng),
-            'synapse_type' : StaticSynapse(weight=g_e),
+            'connector' : FixedProbabilityConnector(.02, allow_self_connections=False, rng=NumpyRNG(1235342134, parallel_safe=False)),
+            'synapse_type' : StaticSynapse(weight=6e-3),
             'receptor_type' : 'excitatory'
         },
         'py_inh' : {
             'source' : 'py',
             'target' : 'inh',
-            'connector' : FixedProbabilityConnector(p_c, allow_self_connections=False, rng=rng),
-            'synapse_type' : StaticSynapse(weight=g_e),
+            'connector' : FixedProbabilityConnector(.02, allow_self_connections=False, rng=NumpyRNG(1235342134, parallel_safe=False)),
+            'synapse_type' : StaticSynapse(weight=6e-3),
             'receptor_type' : 'excitatory'
         },
         'inh_py' : {
             'source' : 'inh',
             'target' : 'py',
-            'connector' : FixedProbabilityConnector(p_c, allow_self_connections=False, rng=rng),
-            'synapse_type' : StaticSynapse(weight=g_i),
+            'connector' : FixedProbabilityConnector(.02, allow_self_connections=False, rng=NumpyRNG(1235342134, parallel_safe=False)),
+            'synapse_type' : StaticSynapse(weight=67e-3),
             'receptor_type' : 'inhibitory'
         },
         'inh_inh' : {
             'source' : 'inh',
             'target' : 'inh',
-            'connector' : FixedProbabilityConnector(p_c, allow_self_connections=False, rng=rng),
-            'synapse_type' : StaticSynapse(weight=g_i),
+            'connector' : FixedProbabilityConnector(.02, allow_self_connections=False, rng=NumpyRNG(1235342134, parallel_safe=False)),
+            'synapse_type' : StaticSynapse(weight=67e-3),
             'receptor_type' : 'inhibitory'
         }
     },
@@ -130,8 +116,14 @@ params = {
 
     'Modifiers' :{
         'py' : {
-            'start' : 0,
-            'end' : .1
+            'cells' : {
+                'start' : 0,
+                'end' : .1
+            },
+            'properties' : {
+                'a' : 0.02e3,
+                'b' : 0.0
+            }
         }
     }
 
