d11334dfdaf445d1e51755be1d719fbd1395fb73
Domenico Guarino <domenico.guarino@gmail.com>
2016-10-28 22:37:19 +0200

    Commented conductances

--- helpers.py
+++ helpers.py
@@ -130,10 +130,10 @@ def analyse(Populations,filename):
             vm = data.filter(name = 'v')[0]
             gsyn_exc = data.filter(name="gsyn_exc")
             gsyn_inh = data.filter(name="gsyn_inh")
-            if not gsyn_exc:
-                gsyn = gsyn_inh[0]
-            else:
-                gsyn = gsyn_exc[0]
+            #if not gsyn_exc:
+            #    gsyn = gsyn_inh[0]
+            #else:
+            #    gsyn = gsyn_exc[0]
                         
             Figure(
                 Panel(vm, ylabel="Membrane potential (mV)",xlabel="Time (ms)", xticks=True,legend = None),
